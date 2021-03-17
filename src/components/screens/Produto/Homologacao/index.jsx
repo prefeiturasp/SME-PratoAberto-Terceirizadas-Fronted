19106e7a10a41e45ffbdc1fb87f157e285406f75
rg3915 <regis42santos@gmail.com>
2021-03-17 16:02:03 -0300

    feature 34176-35676

--- src/components/screens/Produto/Homologacao/index.jsx
+++ src/components/screens/Produto/Homologacao/index.jsx
@@ -45,6 +45,7 @@ class HomologacaoProduto extends Component {
       uuid: null,
       protocoloAnalise: null,
       showModal: false,
+      visible: false,
       qualModal: "indeferir",
       status: null,
       reclamacaoProduto: null,
@@ -53,6 +54,12 @@ class HomologacaoProduto extends Component {
     };
     this.closeModal = this.closeModal.bind(this);
   }
+
+  showModalHist = () => {
+    this.setState({
+      visible: true
+    });
+  };
 
   closeModal = () => {
     this.setState({ showModal: false });
@@ -220,6 +227,133 @@ class HomologacaoProduto extends Component {
                     : undefined
                 }
               />
+
+              <Modal
+          title="Histórico"
+          visible={this.state.visible}
+          onOk={this.handleOk}
+          okText={"Fechar"}
+          onCancel={this.handleCancel}
+          width={800}
+          maskClosable={false}
+        >
+          <section className="body-modal-produto">
+            <div>Usuário</div>
+            <div>Ações</div>
+            <article className="list-logs">
+              <section className="body-logs">
+                {logs.map((log, index) => {
+                  const { ativo } = log;
+                  const iniciais = this.retornaIniciais(log);
+                  const tipoUsuario =
+                    log.usuario.tipo_usuario === "terceirizada"
+                      ? "TERCEIRIZADA"
+                      : "CODAE";
+
+                  return (
+                    <div
+                      key={index}
+                      className={`${ativo && "ativo-item"} grid-item-log`}
+                      onClick={() => {
+                        this.itemLogAtivo(index, ativo);
+                      }}
+                    >
+                      <div className="usuario">
+                        <div>{iniciais}</div>
+                      </div>
+                      <div className="descricao">
+                        <div
+                          className="descicao-titulo"
+                          title={log.status_evento_explicacao}
+                        >
+                          {truncarString(log.status_evento_explicacao, 19)}
+                        </div>
+                        <div className="descicao-entidade">{tipoUsuario}</div>
+                      </div>
+                      <div className="descricao">
+                        <div className="hora">
+                          {log.criado_em.split(" ")[0]}
+                        </div>
+                        <div className="hora">
+                          {log.criado_em.split(" ")[1]}
+                        </div>
+                      </div>
+                    </div>
+                  );
+                })}
+              </section>
+            </article>
+            <article className="detail-log">
+              <div />
+
+              <div>
+                <header>
+                  <div />
+                  {logSelecionado !== null ? (
+                    <div className="descricao-do-log">
+                      <div className="header-log">
+                        <div className="usuario">
+                          <div>{this.retornaIniciais(logSelecionado)}</div>
+                        </div>
+                        <div className="nome-fantasia-empresa">
+                          {logSelecionado.empresa}
+                        </div>
+                        <div>
+                          <div>{logSelecionado.criado_em.split(" ")[0]}</div>
+                          <div>{logSelecionado.criado_em.split(" ")[1]}</div>
+                        </div>
+                      </div>
+                      <div className="body-log-item">
+                        <header>
+                          {logSelecionado.status_evento_explicacao}
+                        </header>
+                        <section>
+                          <article>
+                            <div>
+                              RF: {logSelecionado.usuario.registro_funcional}
+                            </div>
+                            <div className="criado-em">
+                              <div>Data:</div>
+                              <div>
+                                {logSelecionado.criado_em.split(" ")[0]}
+                              </div>
+                            </div>
+                          </article>
+                          <article>
+                            {logSelecionado.justificativa !== "" && (
+                              <>
+                                <div>Justificativa:</div>
+                                <div
+                                  dangerouslySetInnerHTML={{
+                                    __html: logSelecionado.justificativa
+                                  }}
+                                />
+                              </>
+                            )}
+                          </article>
+                        </section>
+                      </div>
+                    </div>
+                  ) : (
+                    <div />
+                  )}
+                </header>
+              </div>
+            </article>
+          </section>
+        </Modal>
+
+              <div className="row col-12">
+                <div className="col-10 ml-0"></div>
+                <div className="col-2 d-flex" style={{ alignItems: "flex-end" }}>
+                  <Botao
+                    type={BUTTON_TYPE.BUTTON}
+                    texto="Histórico"
+                    style={BUTTON_STYLE.GREEN_OUTLINE}
+                    onClick={this.showModal}
+                  />
+                </div>
+              </div>
               {!!status && status === STATUS_CODAE_AUTORIZOU_RECLAMACAO && (
                 <InformativoReclamacao homologacao={ultima_homologacao} />
               )}
